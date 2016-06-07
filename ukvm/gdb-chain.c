16e07129de62bc1f628d14b05cd1b47c29b8dda1
Dan Williams <djwillia@us.ibm.com>
2016-06-07 14:25:48 +0000

    initial checkin

---
+++ ukvm/gdb-chain.c
@@ -0,0 +1,75 @@
+#include "gdb-chain.h"
+
+#include <sys/select.h>
+
+#define MAX_CHAIN_LEN 10
+#define ERROR(x...) do {                        \
+        printf(x);                              \
+        exit(1);                                \
+    } while(0)
+
+int main(int argc, char **argv) {
+    int i;
+    int fd[MAX_CHAIN_LEN];
+    char buf[GDB_CHAIN_BUF_LEN];
+    char *tosend;
+        
+    if ( argc < 3 )
+        ERROR("Usage %s <num> [socket paths...]\n", argv[0]);
+    
+    if ( argc - 2 > MAX_CHAIN_LEN )
+        ERROR("max chain len is %d\n", MAX_CHAIN_LEN);
+
+    for ( i = 0; i < argc - 2; i++ ) {
+        struct sockaddr_un addr;
+
+        printf("setting up [%d] at %s\n", i, argv[i + 2]);
+        
+        fd[i] = socket(AF_UNIX, SOCK_STREAM, 0);
+        if ( fd < 0 )
+            ERROR("couldn't set up socket for %s\n", argv[i + 2]);
+
+        memset(&addr, 0, sizeof(addr));
+        addr.sun_family = AF_UNIX;
+        strncpy(addr.sun_path, argv[i + 2], sizeof(addr.sun_path) - 1);
+        unlink(argv[i + 2]);
+        
+        if ( bind(fd[i], (struct sockaddr *)&addr, sizeof(addr)) )
+            ERROR("couldn't bind to socket for %s\n", argv[i + 2]);
+
+        if ( listen(fd[i], 0) )
+            ERROR("couldn't listen to socket for %s\n", argv[i + 2]);
+    }
+
+    tosend = argv[1];
+    memset(buf, 0, GDB_CHAIN_BUF_LEN);
+
+    for ( i = 0; i < argc - 2; i++ ) {    
+        int ukvm;
+        int ret;
+        int len;
+        
+        printf("waiting to send %s to [%d] at %s\n", tosend, i, argv[i + 2]);
+        ukvm = accept(fd[i], NULL, NULL);
+        if ( ukvm < 0 )
+            ERROR("couldn't accept socket for %s\n", argv[i + 2]);
+
+        len = strlen(tosend);
+        ret = write(ukvm, tosend, len);
+        if ( ret != len ){
+            perror("write");
+            ERROR("couldn't write %s (len %d) ret=%d\n", tosend, len, ret);
+        }
+
+        memset(buf, 0, GDB_CHAIN_BUF_LEN);
+        ret = read(ukvm, buf, sizeof(buf) - 1);
+        if ( ret <= 0 )
+            ERROR("couldn't read (ret %d)\n", ret);
+
+        tosend = buf;
+    }
+
+    
+    
+    return 0;
+}
---
+++ ukvm/gdb-chain.h
@@ -0,0 +1,13 @@
+#ifndef __GDB_CHAIN_H__
+#define __GDB_CHAIN_H__
+
+#include <stdio.h>
+#include <sys/socket.h>
+#include <sys/un.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+#define GDB_CHAIN_BUF_LEN 20
+
+
+#endif
